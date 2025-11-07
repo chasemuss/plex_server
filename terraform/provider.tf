b4d9bd0fadc118a9d9f7e404b8917035b53a4eaf
chasemuss <chasemuss@gmail.com>
2025-11-07 17:04:02 -0600

    Debugging Actions

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "aws" {
   region     = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
 
   # Apply global tags to all resources
   default_tags {
