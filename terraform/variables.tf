6902f0a328f362de3e37d1d91640465e40595fcb
chasemuss <chasemuss@gmail.com>
2025-11-07 20:03:54 -0600

    Removed access key variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,16 @@
 # terraform/variables.tf
 
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
+# variable "aws_access_key" {
+#   description = "AWS Access Key"
+#   type        = string
+#   sensitive   = true
+# }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
+# variable "aws_secret_key" {
+#   description = "AWS Secret Key"
+#   type        = string
+#   sensitive   = true
+# }
 
 variable "region" {
   description = "AWS region to deploy resources"
