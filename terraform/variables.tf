fa204bfe4d75ce5651263787932f20ca2d204daf
chasemuss <chasemuss@gmail.com>
2025-11-08 09:51:29 -0600

    Changed size of instance to t3.micro

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,7 +21,7 @@ variable "region" {
 variable "instance_type" {
   description = "EC2 instance type for Plex"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.micro"
 }
 
 variable "global_tags" {
